4cd4e2f12b9edf935e52e95345bef5f030a81949
TSAGAANTSOOJ TUVSHINSAIKHAN <toutuvshin@gmail.com>
2017-10-30 13:25:03 +0900

    doing

--- app/controllers/nihonshus_controller.rb
+++ app/controllers/nihonshus_controller.rb
@@ -11,38 +11,39 @@ class NihonshusController < ApplicationController
 end
 
 def search
-    # @nihonshus = Nihonshu.all
+    #@nihonshus = Nihonshu.all
 
     # search by price&taste
     include PgSearch
     scope :sorted, ->{ order(price: :asc) }
-    @result = pg_search_scope :search_by_taste_and_price, against: [ :taste, :price ]
+    @result = pg_search_scope :search_by_taste_and_price_and_rating, against: [ :taste, :price, :rating]
 
     # search all
-    @nihonshus =
-    unless search_params.empty?
-      Nihonshu.search(search_params)
-    else
-      nil
-    end
+    @nihonshus = Nihonshu.search(params[:search])
+    # @nihonshus =
+    # unless search_params.empty?
+    #   Nihonshu.search(search_params)
+    # else
+    #   nil
+    # end
   end
 
   def show
     @nihonshu = Nihonshu.find(params[:id])
   end
 
-  def get_barcode    
-     @nihonshu = Nihonshu.find_or_initialize_by(sku: params[:sku])   
-     print @nihonshu   
-     unless @nihonshu.new_record?    
-       # redirect_to nihonshu_path(@nihonshu)    
-      render json: {target_url: nihonshu_path(@nihonshu)}    
-     else    
-      # redirect_to user_path(current_user)  
-      render json: {target_url: root_path}    
-     end   
-   end   
-   
+  def get_barcode
+     @nihonshu = Nihonshu.find_or_initialize_by(sku: params[:sku])
+     print @nihonshu
+     unless @nihonshu.new_record?
+       # redirect_to nihonshu_path(@nihonshu)
+      render json: {target_url: nihonshu_path(@nihonshu)}
+     else
+      # redirect_to user_path(current_user)
+      render json: {target_url: root_path}
+     end
+   end
+
 
   private
 
