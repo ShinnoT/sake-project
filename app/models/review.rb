6cc94a43e0c6589200daa76ddad44fdfcb984af4
TSAGAANTSOOJ TUVSHINSAIKHAN <toutuvshin@gmail.com>
2017-11-02 17:06:57 +0900

    delete title review

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ApplicationRecord
   belongs_to :nihonshu
   belongs_to :user
   # validates :description, length: { minimum: 20 }
-  validates :title, :description, :rating, presence: true
+  # validates :title, :description, :rating, presence: true
+  validates :description, :rating, presence: true
   validates :rating, numericality: { only_float: true }, :inclusion => { :in => 0..5 }
 end
